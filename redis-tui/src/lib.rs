dd02d86c11a5013b66288575e17a415654b137bd
Perry Olsson <olsson.perry@gmail.com>
2025-09-14 10:39:28 -0700

    refactor for multiple test apps

--- redis-tui/src/lib.rs
+++ redis-tui/src/lib.rs
@@ -2,10 +2,14 @@ use crossterm::event::{self, Event};
 use ratatui::{text::Text, Frame};
 
 pub fn run() {
+    run_hello_world();
+}
+
+fn run_hello_world() {
     let mut terminal = ratatui::init();
 
     loop {
-        terminal.draw(draw).expect("Failed to draw frame");
+        terminal.draw(draw_hello).expect("Failed to draw frame");
         if matches!(event::read().expect("Failed to read event"), Event::Key(_)) {
             break;
         }
@@ -14,21 +18,21 @@ pub fn run() {
     ratatui::restore()
 }
 
-pub fn draw(frame: &mut Frame) {
+pub fn draw_hello(frame: &mut Frame) {
     let text = Text::raw("Hello world!");
     frame.render_widget(text, frame.area());
 }
 
 #[cfg(test)]
 mod test {
-    use super::draw;
+    use super::draw_hello;
     use ratatui::{backend::TestBackend, Terminal};
 
     #[test]
     fn simple_hello_world() {
         let backend = TestBackend::new(30, 10);
         let mut terminal = Terminal::new(backend).unwrap();
-        terminal.draw(draw).expect("Failed to draw");
+        terminal.draw(draw_hello).expect("Failed to draw");
 
         terminal.backend().assert_buffer_lines([
             "Hello world!                  ",
--- redis-tui/tests/hello_world.rs
+++ redis-tui/tests/hello_world.rs
@@ -1,12 +1,13 @@
 use crossterm::event::KeyCode;
-use redis_tui::{run, draw};
+use redis_tui::{run, draw_hello};
 use ratatui::{backend::TestBackend, Terminal};
 
 #[test]
+#[ignore]
 fn simple_hello_world() {
     let backend = TestBackend::new(30, 10);
     let mut terminal = Terminal::new(backend).unwrap();
-    terminal.draw(draw).expect("Failed to draw");
+    terminal.draw(draw_hello).expect("Failed to draw");
 
     terminal.backend().assert_buffer_lines([
         "Hello world!                  ",
