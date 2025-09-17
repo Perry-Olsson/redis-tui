4fdfe0ef2fc6552d691fddef5520280a19d45289
Perry Olsson <olsson.perry@gmail.com>
2025-09-17 00:05:41 -0700

    redis index

--- data/seed.lua
+++ data/seed.lua
@@ -2,14 +2,20 @@ local num_users = ARGV[1]
 local num_posts = ARGV[2]
 
 math.randomseed(redis.call("TIME")[1])
+redis.call("SET", "user_id", 0)
 for i = 1, tonumber(num_users) do
     local randomNumber = math.random(15, 40)
-    redis.call("HSET", "user:"..i, "name", "John Doe | "..i, "email", "john_"..i.."@gmail.com", "age", randomNumber);
+    local id = redis.call("INCR", "user_id")
+    redis.call("HSET", "user:"..id, "name", "John Doe | "..id, "email", "john_"..id.."@gmail.com", "age", randomNumber);
 end
 
+redis.call("SET", "post_id", 0)
 for i = 1, tonumber(num_posts) do
     local randomNumber = math.random(1, 6)
-    redis.call("HSET", "post:"..i, "title", "Post Title | "..i, "content", "content | "..i, "user", "user:"..randomNumber)
+    local id = redis.call("INCR", "post_id")
+    redis.call("HSET", "post:"..id, "title", "Post Title | "..id, "content", "content | "..id, "likes", math.random(0, 5000), "user", ""..randomNumber)
 end
+
+redis.call("FT.CREATE", "post", "ON", "HASH", "PREFIX", "1", "post:", "SCHEMA", "likes", "NUMERIC", "SORTABLE", "user", "NUMERIC")
 
 return "OK"
